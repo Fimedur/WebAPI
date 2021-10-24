e30f8e11e33d0501bc939596728a554613dc6ef5
Fimedur <fimedur.rahman.shuvo@gmail.com>
2021-10-24 18:51:13 +0600

    SQL view test work ok

---
+++ WebAPI/Controllers/JoinTableController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebAPI.Model;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JoinTableController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public JoinTableController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                  SELECT  [DepartmentId]
+                         ,[DepartmentName]
+                         ,[EmployeeId]
+                         ,[EmployeeName]
+                         ,convert(varchar(10),DateOfJoining,120) as DateOfJoining
+                         FROM [EmoloyeDB].[dbo].[JoinTable]";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+    }
+}
